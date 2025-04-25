e58b227f7861942a4b76911e71e8b4c7359bcadc
LuigiMazza <88099926+LuigiMazza@users.noreply.github.com>
2025-04-24 23:05:52 -0300

    Delete Nueva carpeta directory

--- Nueva carpeta/script.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.getElementById('burger-menu');
-const menuList = document.getElementById('menu-list');
-
-burgerMenu.addEventListener('click', () => {
-    menuList.classList.toggle('active'); // Alterna la clase 'active' para mostrar/ocultar el menú
-});
